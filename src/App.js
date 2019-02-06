f19ef712a5db52fc3535a9936f11aae8451fa2ff
Varun <shrva02@ca.com>
2019-02-06 23:37:07 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import AutoComplete from './components/AutoComplete'
+//import DataTable from './components/DataTable'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const arrayA=["abc","bcd","aa","de"]
+const arrayB=["ABC","BCD","AA","DE",'AAAAAA']
+export default class App extends Component{
+ 
+  constructor(props){
+    super(props)
+    this.handleSuggestion1 = this.handleSuggestion1.bind(this);
   }
+  
+  state={suggestion1:arrayA,
+  selectedVal:null,
+  state2:"",state3:"",state4:""}
+  
+  handleSuggestion1(selectedValue){
+    console.log('selectedValue...',selectedValue)
+    this.setState({suggestion1:arrayB});
+  }
+    render(){
+      return(
+        <div className="container">
+        <div className="col-sm"><AutoComplete suggestions={this.state.suggestion1} handleSuggestion={this.props.handleSuggestion1} />
+        </div>
+        
+       
+        </div>
+      )
+    }
 }
-
-export default App;
